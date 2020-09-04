11d93ed9e7305b4acfe6e8b07805dec36cb84605
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-04 17:00:59 -0600

    began adding todos stuff

--- app/Controllers/TodoController.js
+++ app/Controllers/TodoController.js
@@ -1,19 +1,24 @@
 import todoService from "../Services/TodoService.js";
+import { ProxyState } from "../AppState.js";
 
 //TODO Create the draw function
-function _drawTodos() { }
-
+function _drawTodos() {
+  let template = "";
+  ProxyState.todos.forEach((td) => (template += td.Template));
+  document.getElementById("todos-display").innerHTML = template;
+}
 export default class TodoController {
   constructor() {
     //TODO Remember to register your subscribers
+    ProxyState.on("todos", _drawTodos);
     todoService.getTodos();
   }
 
   getTodos() {
     try {
-      todoService.getTodos()
+      todoService.getTodos();
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   }
   addTodo(e) {
@@ -24,31 +29,31 @@ export default class TodoController {
     try {
       todoService.addTodo(todo);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   }
 
   /**
- * This method takes in an id of the Todo that should be togggled as complete
- * @param {string} todoId 
- */
+   * This method takes in an id of the Todo that should be togggled as complete
+   * @param {string} todoId
+   */
   toggleTodoStatus(todoId) {
     try {
       todoService.toggleTodoStatus(todoId);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   }
 
   /**
    * This method takes in an id of the Todo that should be removed
-   * @param {string} todoId 
+   * @param {string} todoId
    */
   removeTodo(todoId) {
     try {
       todoService.removeTodo(todoId);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   }
 }
--- app/models/Todo.js
+++ app/models/Todo.js
@@ -1,5 +1,12 @@
 export default class Todo {
   constructor({ id }) {
+    console.log(id);
     this.id = id;
+  }
+
+  get Template() {
+    return `
+    <p>${this.id}</p>
+    `;
   }
 }
