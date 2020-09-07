3783ba5d912db37dd414a00707542e348092548c
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-07 14:47:31 -0600

    added greeting per time of day

--- app/main.js
+++ app/main.js
@@ -21,6 +21,8 @@ function clock() {
   let hourDisplay = h;
   if (h > 12) {
     hourDisplay = h - 12;
+  }
+  if (h >= 12) {
     amOrPm = "pm";
   }
   if (h == 12 && m == 0) {
@@ -34,10 +36,55 @@ function clock() {
   if (h == 0) {
     hourDisplay = 12;
   }
+  let greeting = "";
+  switch (h) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+      greeting = "What are you doing up? 🙃";
+      break;
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+      greeting = "Good Morning ☕";
+      break;
+    case 11:
+      greeting = "Getting time for lunch 🍔";
+      break;
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+      greeting = "Good Afternoon ☀️";
+      break;
+    case 17:
+      greeting = "Hmm, 5:00 😉 🍻";
+      break;
+    case 18:
+    case 19:
+      greeting = "Good Evening 🍻🍻🍻";
+      break;
+    case 20:
+    case 21:
+      greeting = "Getting Late... 📺 ";
+      break;
+    case 22:
+    case 23:
+      greeting = "Gettin' Real Late... 😴 ";
+      break;
+  }
 
   m = checkTime(m);
+
   document.getElementById("clock").innerHTML =
     hourDisplay + ":" + m + " " + amOrPm;
+  document.getElementById("greeting").innerHTML = greeting;
   let t = setTimeout(clock, 500);
 }
 
