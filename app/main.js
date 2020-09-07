aff16751593dfe9d0d90dc1b074bfbd7bae6a55e
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-07 10:51:37 -0600

    got clock running

--- app/main.js
+++ app/main.js
@@ -10,7 +10,42 @@ class App {
     this.todoController = new TodoController();
     this.quoteController = new QuoteController();
     this.imageController = new ImageController();
+add    clock();
   }
+}
+function clock() {
+  let now = new Date();
+  let h = now.getHours();
+  let m = now.getMinutes();
+  let amOrPm = "am";
+  let hourDisplay = h;
+  if (h > 12) {
+    hourDisplay = h - 12;
+    amOrPm = "pm";
+  }
+  if (h == 12 && m == 0) {
+    amOrPm = "noon";
+  }
+
+  if (h == 0 && m == 0) {
+    amOrPm = "midnight";
+  }
+
+  if (h == 0) {
+    hourDisplay = 12;
+  }
+
+  m = checkTime(m);
+  document.getElementById("clock").innerHTML =
+    hourDisplay + ":" + m + " " + amOrPm;
+  let t = setTimeout(clock, 500);
+}
+
+function checkTime(timeUnit) {
+  if (timeUnit < 10) {
+    timeUnit = "0" + timeUnit;
+  } // add zero in front of numbers < 10
+  return timeUnit;
 }
 
 window["app"] = new App();
