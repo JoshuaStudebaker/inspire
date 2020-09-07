22d1bd3938879e73b1dd121342eead02bfbd46ea
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-07 08:34:21 -0600

    created author hover

--- app/models/Quote.js
+++ app/models/Quote.js
@@ -6,8 +6,8 @@ export default class Quote {
   }
   get Template() {
     return `
-    <p class="text-right mb-0">"<i>${this.body}</i>"</p>
-    <p class="text-right"> - ${this.author}</p>
+    <p class="text-right mb-0 author-hover">"<i>${this.body}</i>"</p>
+    <p class="text-right author-hide"> - ${this.author}</p>
     
     `;
   }
