26b6e39be4fe3b4921b7076b573e6cb3ce8d4bfd
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-07 21:09:31 -0600

    forgot to lowercase models in the appstate

--- app/AppState.js
+++ app/AppState.js
@@ -1,7 +1,7 @@
-import Todo from "./Models/Todo.js";
-import Weather from "./Models/Weather.js";
-import Quote from "./Models/Quote.js";
-import Image from "./Models/Image.js";
+import Todo from "./models/Todo.js";
+import Weather from "./models/Weather.js";
+import Quote from "./models/Quote.js";
+import Image from "./models/Image.js";
 import { EventEmitter } from "./Utils/EventEmitter.js";
 import { isValidProp } from "./Utils/isValidProp.js";
 
